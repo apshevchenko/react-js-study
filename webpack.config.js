e9043077bf1f533e1bb7dfae61a32afe7a525b1d
artem <apshevchenko@gmail.com>
2021-04-10 15:16:30 +0300

    add typescript babel webpack jest eslint prettier husky

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "index.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)x?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
