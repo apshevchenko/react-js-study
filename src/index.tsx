7f957a5ebcb554979462890c8080685f367fb78b
artem <apshevchenko@gmail.com>
2021-04-27 22:07:12 +0300

    Add index files

---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { ListComponent } from './components/List/ListComponent'
+
+render(<ListComponent list={[1, 2, 3, 4, 5]} listItemHandleClick={(id) => { console.log(id) }} />, document.getElementById('root'))
