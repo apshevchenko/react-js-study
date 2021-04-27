df9dff5fa6590bd82c6b001c6efe3098a2866dd9
artem <apshevchenko@gmail.com>
2021-04-27 22:03:30 +0300

    Add components stories

---
+++ src/components/List/ListItemComponent.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Story } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { ListItemComponent, ListItemProps } from './ListItemComponent'
+
+export default {
+  title: 'ListItemComponent',
+  component: ListItemComponent,
+  argTypes: {
+    id: { control: { type: 'number' }, defaultValue: 1 },
+    children: {
+      control: { type: 'text' }, defaultValue: 1
+    }
+  }
+}
+
+export const ListItemComponentStory: Story<ListItemProps> = (args) =>
+  <ListItemComponent {...args} listItemHandleClick={action('ListItemClick')} />
