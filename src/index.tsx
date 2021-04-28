eea100331e2db0f73e34e1db4244cbfcadb08dff
artem <apshevchenko@gmail.com>
2021-04-28 17:14:41 +0300

    simplify handleClick func in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,11 +3,6 @@ import { render } from "react-dom";
 import { ListComponent } from "./components/List/ListComponent";
 
 render(
-  <ListComponent
-    list={[1, 2, 3, 4, 5]}
-    listItemHandleClick={(id) => {
-      console.log(id);
-    }}
-  />,
+  <ListComponent list={[1, 2, 3, 4, 5]} listItemHandleClick={console.log} />,
   document.getElementById("root")
 );
